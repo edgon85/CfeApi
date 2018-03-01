d9a572dcfaea2c472bb2535399481628a28b2d5f
edgon <edgon85@gmail.com>
2018-02-28 23:44:34 -0600

    Serializer Data

--- modules/updates/views.py
+++ modules/updates/views.py
@@ -2,8 +2,11 @@ import json
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 from django.views.generic import View
+from django.core.serializers import serialize
 
 from cfeapi.mixins import JsonResponseMixin
+
+from .models import Update
 
 # Create your views here.
 
@@ -35,5 +38,32 @@ class JsonCBV2(JsonResponseMixin, View):
             "content": "Some new data"
         }
         return self.render_to_json_response(data)
+
+
+class SerializedDetailView(View):
+    def get(self, request, *args, **kwargs):
+        obj = Update.objects.get(id=1)
+        data = serialize("json", [obj,], fields=('user','content'))
+        json_data = data
+
+        # data = {
+        #     "user": obj.user.username 
+        #     "content": obj.content
+        # }
+        return HttpResponse(json_data, content_type='application/json')
+
+
+class SerializedListView(View):
+    def get(self, request, *args, **kwargs):
+        queryset = Update.objects.all()
+        # data = serialize("json", queryset)
+        data = serialize("json", queryset, fields=('user','content'))
+        print(data)
+        json_data = data
+        # data = {
+        #     "user": obj.user.username 
+        #     "content": obj.content
+        # }
+        return HttpResponse(json_data, content_type='application/json')
 
 
