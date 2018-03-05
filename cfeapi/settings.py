7dafc4fff529744d3574ff137094ef7e02a957c5
edgon <edgon85@gmail.com>
2018-03-04 19:44:48 -0600

    Created status app and install django-rest-framework, status model & app

--- cfeapi/settings.py
+++ cfeapi/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'modules.updates'
+    
+    #Third party
+    'rest_framework',
+    #local
+    'modules.updates',
+    'modules.status',
 ]
 
 MIDDLEWARE = [
