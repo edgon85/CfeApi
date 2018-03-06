d1c509860cfbfd764475d50bf44a2343febc7830
edgon <edgon85@gmail.com>
2018-03-06 11:01:04 -0600

    validation & fields

--- modules/status/api/serializer.py
+++ modules/status/api/serializer.py
@@ -15,3 +15,13 @@ class StatusSerializer(serializers.ModelSerializer):
             'image'
         ]
 
+    def validate(self, data):
+        content = data.get("content", None)
+        if content == "":
+            content = None
+        image = data.get("image", None)
+        if content is None and image is None:
+            raise serializers.ValidationError("Content or image is required.")
+        return data
+
+
