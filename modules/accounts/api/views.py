e7dbd8c19de42af96c3f3a96ba215fcfc061ac8a
Edgon <edgon@iMac-de-JOME.local>
2018-03-11 13:42:02 -0600

    User register serializer

--- modules/accounts/api/views.py
+++ modules/accounts/api/views.py
@@ -2,10 +2,12 @@ from django.contrib.auth import authenticate, get_user_model
 
 from django.db.models import Q
 
-from rest_framework import permissions
+from rest_framework import permissions, generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework_jwt.settings import api_settings
+
+from .serializer import UserRegisterSerializer
 
 
 jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
@@ -42,33 +44,39 @@ class AuthAPIView(APIView):
         return Response({"Detail": "Invalid credentials"}, status=401)
 
 
-class RegisterAPIView(APIView):
-    permission_classes = []
-    
-    def post(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            return Response({'detail':'You are already registered and are authenticated.'}, status=400)
-        data = request.data
-        username        = data.get('username')
-        email           = data.get('username')
-        password        = data.get('password')
-        password2       = data.get('password2')
+class RegisterAPIView(generics.CreateAPIView):
+    queryset                = User.objects.all()
+    serializer_class        = UserRegisterSerializer
+    permission_classes      = [permissions.AllowAny]
 
-        qs = User.objects.filter(
-                Q(username__iexact=username) |
-                Q(email__iexact=username)
-            )
-        if password != password2:
-            return Response({'password':'Password must match'}, status=401)
-        if qs.exists():
-            return Response ({'return':'This user already exists'}, status=401)
-        else:
-            user = User.objects.create(username=username, email=email)
-            user.set_password(password)
-            user.save()
-            # payload = jwt_payload_handler(user)
-            # token = jwt_encode_handler(payload)
-            # response = jwt_response_payload_handler(token, user, request=request)
-            # return Response(response, status=201)
-            return Response({'detail':'Thank you for registering. Please verify your email.'})
-        return Response({'detail':'Invalid request'}, status=400)
+
+# class RegisterAPIView(APIView):
+#     permission_classes = [permissions.AllowAny]
+    
+#     def post(self, request, *args, **kwargs):
+#         if request.user.is_authenticated:
+#             return Response({'detail':'You are already registered and are authenticated.'}, status=400)
+#         data = request.data
+#         username        = data.get('username')
+#         email           = data.get('username')
+#         password        = data.get('password')
+#         password2       = data.get('password2')
+
+#         qs = User.objects.filter(
+#                 Q(username__iexact=username) |
+#                 Q(email__iexact=username)
+#             )
+#         if password != password2:
+#             return Response({'password':'Password must match'}, status=401)
+#         if qs.exists():
+#             return Response ({'return':'This user already exists'}, status=401)
+#         else:
+#             user = User.objects.create(username=username, email=email)
+#             user.set_password(password)
+#             user.save()
+#             # payload = jwt_payload_handler(user)
+#             # token = jwt_encode_handler(payload)
+#             # response = jwt_response_payload_handler(token, user, request=request)
+#             # return Response(response, status=201)
+#             return Response({'detail':'Thank you for registering. Please verify your email.'})
+#         return Response({'detail':'Invalid request'}, status=400)
